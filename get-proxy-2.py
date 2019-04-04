db8d29eb90d3fc7265c3cde098154aa1a5d4488e
StevenJoeZhang <1119186082@qq.com>
2019-04-04 13:33:12 +0800

    Format code

--- get-proxy-2.py
+++ get-proxy-2.py
@@ -16,56 +16,56 @@ sys.setdefaultencoding('utf-8')
 url = 'http://www.xicidaili.com/nn/%s/'
 
 def findProxy():
-    proxys = []
-    print '开始从%s上查找代理' % url
-    for i in range(1,10):
-        tmp = spider_xicidaili(i)
-        proxys += tmp
-    writeFile(proxys)
-    print '共找到%s个代理' % len(proxys)
+	proxys = []
+	print '开始从%s上查找代理' % url
+	for i in range(1,10):
+		tmp = spider_xicidaili(i)
+		proxys += tmp
+	writeFile(proxys)
+	print '共找到%s个代理' % len(proxys)
 
 def spider_xicidaili(index):
-    headers = ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
-    opener = urllib2.build_opener()
-    opener.addheaders = [headers]
-    try:
-        urls = []
-        page = opener.open(url % index).read()
-        page = BeautifulSoup(page)
-        proxys = page.table.findAll('tr')
-        for p in proxys:
-            if p.findAll('th'):
-                continue;
-            else:
-                td = p.findAll('td')
-            host, port = td[1].string, td[2].string
-            print host, port
-            urls.append('http://%s:%s/' % (host, port))
-        return urls
-    except Exception, e:
-        print e
-        return []
+	headers = ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
+	opener = urllib2.build_opener()
+	opener.addheaders = [headers]
+	try:
+		urls = []
+		page = opener.open(url % index).read()
+		page = BeautifulSoup(page)
+		proxys = page.table.findAll('tr')
+		for p in proxys:
+			if p.findAll('th'):
+				continue;
+			else:
+				td = p.findAll('td')
+			host, port = td[1].string, td[2].string
+			print host, port
+			urls.append('http://%s:%s/' % (host, port))
+		return urls
+	except Exception, e:
+		print e
+		return []
 
 def writeFile(proxys):
-    with open('result.txt', 'w') as f:
-        for p in proxys:
-            f.write(str(p) + '\n')
+	with open('result.txt', 'w') as f:
+		for p in proxys:
+			f.write(str(p) + '\n')
 
 def testIP(url):
-    pro = urllib2.ProxyHandler({'http': url})
-    opener = urllib2.build_opener(pro, urllib2.HTTPHandler)
-    urllib2.install_opener(opener)
-    try:
-        content = urllib2.urlopen('http://www.baidu.com').read()
-        return True
-    except Exception, e:
-        print e
-        return False
-    return False
+	pro = urllib2.ProxyHandler({'http': url})
+	opener = urllib2.build_opener(pro, urllib2.HTTPHandler)
+	urllib2.install_opener(opener)
+	try:
+		content = urllib2.urlopen('http://www.baidu.com').read()
+		return True
+	except Exception, e:
+		print e
+		return False
+	return False
 
 def test_spider_xicidaili():
-    spider_xicidaili(1)
+	spider_xicidaili(1)
 
 if __name__ == "__main__":
-    #test_spider_xicidaili()
-    findProxy()
+	#test_spider_xicidaili()
+	findProxy()
--- get-proxy.py
+++ get-proxy.py
@@ -17,54 +17,54 @@ sys.setdefaultencoding('utf-8')
 url = 'https://www.kuaidaili.com/free/inha/%s/'
 
 def findProxy():
-    proxys = []
-    print '开始从%s上查找代理' % url
-    for i in range(1, 10):
-        tmp = spider_kuaidaili(i)
-        proxys += tmp
-        time.sleep(1)
-    writeFile(proxys)
-    print '共找到%s个代理' % len(proxys)
+	proxys = []
+	print '开始从%s上查找代理' % url
+	for i in range(1, 10):
+		tmp = spider_kuaidaili(i)
+		proxys += tmp
+		time.sleep(1)
+	writeFile(proxys)
+	print '共找到%s个代理' % len(proxys)
 
 def spider_kuaidaili(index):
-    headers = ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
-    opener = urllib2.build_opener()
-    opener.addheaders = [headers]
-    try:
-        urls = []
-        page = opener.open(url % index).read()
-        page = BeautifulSoup(page)
-        proxys = page.findAll('tbody')[0].findAll('tr')
-        for p in proxys:
-            td = p.findAll('td')
-            host, port = td[0].string, td[1].string
-            print host, port
-            urls.append('http://%s:%s/' % (host, port))
-        return urls
-    except Exception, e:
-        print e
-        return []
+	headers = ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
+	opener = urllib2.build_opener()
+	opener.addheaders = [headers]
+	try:
+		urls = []
+		page = opener.open(url % index).read()
+		page = BeautifulSoup(page)
+		proxys = page.findAll('tbody')[0].findAll('tr')
+		for p in proxys:
+			td = p.findAll('td')
+			host, port = td[0].string, td[1].string
+			print host, port
+			urls.append('http://%s:%s/' % (host, port))
+		return urls
+	except Exception, e:
+		print e
+		return []
 
 def writeFile(proxys):
-    with open('result.txt', 'w') as f:
-        for p in proxys:
-            f.write(str(p) + '\n')
+	with open('result.txt', 'w') as f:
+		for p in proxys:
+			f.write(str(p) + '\n')
 
 def testIP(url):
-    pro = urllib2.ProxyHandler({'http': url})
-    opener = urllib2.build_opener(pro, urllib2.HTTPHandler)
-    urllib2.install_opener(opener)
-    try:
-        content = urllib2.urlopen('http://www.baidu.com').read()
-        return True
-    except Exception, e:
-        print e
-        return False
-    return False
+	pro = urllib2.ProxyHandler({'http': url})
+	opener = urllib2.build_opener(pro, urllib2.HTTPHandler)
+	urllib2.install_opener(opener)
+	try:
+		content = urllib2.urlopen('http://www.baidu.com').read()
+		return True
+	except Exception, e:
+		print e
+		return False
+	return False
 
 def test_spider_kuaidaili():
-    spider_kuaidaili(1)
+	spider_kuaidaili(1)
 
 if __name__ == "__main__":
-    #test_spider_kuaidaili()
-    findProxy()
+	#test_spider_kuaidaili()
+	findProxy()
